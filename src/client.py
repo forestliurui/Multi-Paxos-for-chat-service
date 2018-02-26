a35ae32a5ddfa15677039634ab32ad2fdc33dcb6
Rui Liu <ruixliu@bigdata.eecs.umich.edu>
2018-02-26 18:10:48 -0500

    some update

--- src/client.py
+++ src/client.py
@@ -8,17 +8,19 @@ from messenger import print_message
 
 timeout = 20
 
-def client(client_idx, config_file_server):
-
-    with open(config_file_server, 'r') as config_handler:
-        config = yaml.load(config_handler)
-    f = int(config['f']) #the number of failure that can be tolerated
-    num_server = 2*f + 1
-    servers_list = { server_idx: config['servers_list'][server_idx] for server_idx in range(num_server)}
+def client(client_idx, config_file_server = None):
 
     host_name = 'bigdata.eecs.umich.edu'
-    servers_list = {idx:{'host': host_name, 'port': 50000+idx} for idx in range(5)}
-    #clients_list = {idx:{'host': host_name, 'port': 40000+idx} for idx in range(50)}
+    servers_list = {idx:{'host': host_name, 'port': 50000+idx} for idx in range(5)} 
+
+    if config_file_server is not None:
+        with open(config_file_server, 'r') as config_handler:
+            config = yaml.load(config_handler)
+        f = int(config['f']) #the number of failure that can be tolerated
+        num_server = 2*f + 1
+        servers_list = { server_idx: config['servers_list'][server_idx] for server_idx in range(num_server)}
+    else:
+        clients_list = {idx:{'host': host_name, 'port': 40000+idx} for idx in range(50)}
       
     client_idx = int(client_idx)
     client_host = clients_list[client_idx]['host']
