476d0ed62aed08ac772bb5536cd03524a5a8ad34
Rui Liu <ruixliu@bigdata.eecs.umich.edu>
2018-02-26 20:17:51 -0500

    create config file for each test case

--- src/start_clients.sh
+++ src/start_clients.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-client_num=2
+client_num=$1
+config_file_server=$2
 
 for ((i=0;i<${client_num};i=i+1))
 do
     echo "start client process: ${i}"
-    python client.py ${i} &
+    python client.py ${i} ${config_file_server} &
 done
 
